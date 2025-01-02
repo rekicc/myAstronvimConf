cee1828af2dc57ad2f9d3966669c7cc474bb4906
rcc <you@example.com>
2025-01-03 00:20:48 +0800

    添加了一些astrocommunity插件
    1. python语言相关组件
    2. Go语言相关组件
    3. twilight, 可以将除当前编辑的代码块外的其他代码颜色变暗
    4. tint将非焦点窗口颜色变暗
    5. transparent-nvim, 将colorscheme的背景色变成透明, 不过找到其他设置透明的方法了, 所以没有实际启用

--- lua/community.lua
+++ lua/community.lua
@@ -1,4 +1,4 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- AstroCommunity: import any community modules here
 -- We import this file in `lazy_setup.lua` before the `plugins/` folder.
@@ -9,4 +9,9 @@ return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
   -- import/override with your plugins folder
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.color.twilight-nvim" },
+  { import = "astrocommunity.color.tint-nvim" },
+  -- { import = "astrocommunity.color.transparent-nvim" },
 }
