191361ac537101bc42258d4adda025fb8b4e2ce0
rcc <you@example.com>
2025-01-02 17:52:38 +0800

    添加插件备份文件: backup.lua
    backup.lua用于记录曾用过的插件, 文件中的所有插件都不会启用, 只是留作记录

---
+++ lua/plugins/backup.lua
@@ -0,0 +1,48 @@
+if true then return {} end
+
+------------安装过的插件记录,留作后用--------------------------------------------------------
+--- Go高亮插件,不会使用
+--{ "ray-x/aurora" },
+
+--- 另一款Go高亮插件.也无法正常工作,可能与NvChad配置有关
+--{ "charlespascoe/vim-go-syntax" },
+
+--- 查找工具, 没有实际启用
+--{ "Yggdroot/LeaderF", lazy=true},
+
+--- go语言另一款插件,和vim比较合适
+--{ "fatih/vim-go",  },
+
+--- folke大神的插件,好像是用于开发neovim插件的插件,没搞懂怎么用
+--{ "folke/lazydev.nvim", ft = "lua" },
+
+--- customize alpha options
+-- {
+--   "goolord/alpha-nvim",
+--   opts = function(_, opts)
+--     -- customize the dashboard header
+--     opts.section.header.val = {
+--       " █████  ███████ ████████ ██████   ██████",
+--       "██   ██ ██         ██    ██   ██ ██    ██",
+--       "███████ ███████    ██    ██████  ██    ██",
+--       "██   ██      ██    ██    ██   ██ ██    ██",
+--       "██   ██ ███████    ██    ██   ██  ██████",
+--       " ",
+--       "    ███    ██ ██    ██ ██ ███    ███",
+--       "    ████   ██ ██    ██ ██ ████  ████",
+--       "    ██ ██  ██ ██    ██ ██ ██ ████ ██",
+--       "    ██  ██ ██  ██  ██  ██ ██  ██  ██",
+--       "    ██   ████   ████   ██ ██      ██",
+--     }
+--     return opts
+--   end,
+-- },
+
+---
+-- { "voldikss/vim-floaterm" },
+
+--- 加载插件自定义的textobjects
+--{ "nvim-treesitter/nvim-treesitter-textobjects", lazy = false },
+
+--- 同时替换开括号与闭括号
+--{ "kylechui/nvim-surround", version = "*", event = "VeryLazy" },
