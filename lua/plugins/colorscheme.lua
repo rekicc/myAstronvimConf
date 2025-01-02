093794eb8c005f27a7a49906f1f348cf283abc2f
rcc <you@example.com>
2025-01-02 14:23:53 +0800

    添加主题: catppuccin
    1. 根据onedark的文档将部分文本的字形修改成了斜体
    2. 添加了catppuccin主题, 不过看起来不如onedark, 所以还是换回onedark了,
    不过同时保留了catppuccin的spec, 只是disable了, 没有删除

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -5,8 +5,29 @@ return {
       highlights = {
         PmenuSel = { bg = "#1A5CA4" },
       },
+      styles = {
+        conditionals = "italic",
+        virtual_text = "italic",
+        comments = "italic",
+      },
       options = {
         transparency = true,
+        -- highlights_inactive_windows = true,
+      },
+    },
+  },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    enabled = false,
+    opts = {
+      transparent_background = true,
+      dim_inactive = {
+        enabled = true,
+      },
+      styles = {
+        comments = { "italic" },
+        conditionals = { "italic" },
       },
     },
   },
