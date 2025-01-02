0a5adee90ebd2c4f1cb7e36bd1a6caeea2218442
rcc <you@example.com>
2025-01-03 00:13:17 +0800

    添加telescope相关扩展
    将telescope设置为lazyload, 同时添加了按频率搜索和对fzf的原生支持
    还添加了对luasnip的搜索, 不过并未启用, 处于lazyload模式

---
+++ lua/plugins/telescopeAbout.lua
@@ -0,0 +1,38 @@
+return {
+  {
+    "nvim-telescope/telescope.nvim",
+    lazy = true,
+    opts = {
+      extensions = {
+        fzf = {
+          fuzzy = true, -- false will only do exact matching
+          override_generic_sorter = true, -- override the generic sorter
+          override_file_sorter = true, -- override the file sorter
+          case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+          -- the default case_mode is "smart_case"
+        },
+      },
+    },
+    dependencies = {
+      { "nvim-telescope/telescope-fzf-native.nvim" },
+      { "nvim-telescope/telescope-frecency.nvim" },
+    },
+  },
+  {
+    "nvim-telescope/telescope-frecency.nvim",
+    lazy = true,
+    config = function() require("telescope").load_extension "frecency" end,
+  },
+  {
+    "nvim-telescope/telescope-fzf-native.nvim",
+    lazy = true,
+    build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release",
+    config = function() require("telescope").load_extension "fzf" end,
+  },
+  --懒加载,未使用,要启用的话需要加到telescope的dependencies中,或者手动load
+  {
+    "benfowler/telescope-luasnip.nvim",
+    lazy = true,
+    config = function() require("telescope").load_extension "luasnip" end,
+  },
+}
