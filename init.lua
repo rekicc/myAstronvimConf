158fb949a7fab1926bec91913a396e6475b65901
rcc <you@example.com>
2025-01-08 12:46:14 +0800

    添加插件:nvchad-ui、base46、volt
    未启用

--- init.lua
+++ init.lua
@@ -1,5 +1,8 @@
 -- This file simply bootstraps the installation of Lazy.nvim and then calls other files for execution
 -- This file doesn't necessarily need to be touched, BE CAUTIOUS editing this file and proceed at your own risk.
+--
+-- vim.g.base46_cache = vim.fn.stdpath "data" .. "/base46_cache/"
+--
 local lazypath = vim.env.LAZY or vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 if not (vim.env.LAZY or (vim.uv or vim.loop).fs_stat(lazypath)) then
   -- stylua: ignore
@@ -17,3 +20,7 @@ end
 
 require "lazy_setup"
 require "polish"
+
+-- for _, v in ipairs(vim.fn.readdir(vim.g.base46_cache)) do
+--   dofile(vim.g.base46_cache .. v)
+-- end
---
+++ lua/plugins/nvchad.lua
@@ -0,0 +1,16 @@
+if true then return {} end
+return {
+  {
+    "nvchad/ui",
+    config = function() require "nvchad" end,
+  },
+
+  {
+    "nvchad/base46",
+    lazy = false,
+    build = function() require("base46").load_all_highlights() end,
+  },
+
+  "nvchad/volt", -- optional, needed for theme switcher
+  -- or just use Telescope themes
+}
