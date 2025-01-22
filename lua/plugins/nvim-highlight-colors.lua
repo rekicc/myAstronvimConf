5333b69d52e44011f88ad46c56d20b78e50b530f
rcc <you@example.com>
2025-01-22 12:54:36 +0800

    插件配置变更：nvim-highlight-colors
    在文件中以虚拟文本的方式显示对应的颜色

---
+++ lua/plugins/nvim-highlight-colors.lua
@@ -0,0 +1,15 @@
+-- if true then return {} end
+---@type LazySpec
+return {
+  "brenoprata10/nvim-highlight-colors",
+  opts = {
+    virtual_symbol = "󱓻",
+    virtual_symbol_suffix = " ",
+    render = "virtual",
+    virtual_symbol_position = "inline",
+    enable_tailwind = true,
+    enable_short_hex = false,
+    enable_named_colors = false,
+    enable_hex = true,
+  },
+}
