975790c3432ef857dac6be4ff27784df83e80fc4
reki <ccreki@gmail.com>
2025-05-20 17:35:41 +0800

    自动切换输入法
    linux下rime无法自动切换中引文输入，所以还是引入了插件进行中英文输入法切换

--- lua/plugins/im-select.lua
+++ lua/plugins/im-select.lua
@@ -1,9 +1,13 @@
-if true then return {} end
+-- if true then return {} end
 return {
   "keaising/im-select.nvim",
   lazy = false,
   opts = {
-    default_im_select = "com.apple.keylayout.ABC",
+    default_im_select = "keyboard-us",
+    default_command = "fcitx5-remote",
     keep_quiet_on_no_binary = true,
+    set_default_events = { "VimEnter", "FocusGained", "InsertLeave", "CmdlineLeave" },
+    set_previous_events = {"InsertEnter"},
+    async_switch_im = true,
   },
 }
