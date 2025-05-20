b15ae189557cf8ead79a620f398e10cc0ae7bd5a
reki <ccreki@gmail.com>
2025-05-20 17:34:29 +0800

    blink.cmp补全源改动
    blink.cmp升级后cmdline以及terminal源的配置方式变更成了当前的配置

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -55,14 +55,6 @@ return {
       -- with blink.compat
       compat = {},
       default = { "lsp", "path", "snippets", "buffer" },
-      cmdline = function()
-        local type = vim.fn.getcmdtype()
-        -- Search forward and backward
-        if type == "/" or type == "?" then return { "buffer" } end
-        -- Commands
-        if type == ":" or type == "@" then return { "cmdline" } end
-        return {}
-      end,
       min_keyword_length = 0,
       providers = {
         lsp = {
@@ -105,6 +97,61 @@ return {
             end, items)
           end,
         },
+      },
+    },
+    cmdline = {
+      enabled = true,
+      -- use 'inherit' to inherit mappings from top level `keymap` config
+      keymap = { preset = "cmdline" },
+      sources = function()
+        local type = vim.fn.getcmdtype()
+        -- Search forward and backward
+        if type == "/" or type == "?" then return { "buffer" } end
+        -- Commands
+        if type == ":" or type == "@" then return { "cmdline" } end
+        return {}
+      end,
+      completion = {
+        trigger = {
+          show_on_blocked_trigger_characters = {},
+          show_on_x_blocked_trigger_characters = {},
+        },
+        list = {
+          selection = {
+            -- When `true`, will automatically select the first item in the completion list
+            preselect = true,
+            -- When `true`, inserts the completion item automatically when selecting it
+            auto_insert = true,
+          },
+        },
+        -- Whether to automatically show the window when new completion items are available
+        menu = { auto_show = false },
+        -- Displays a preview of the selected item on the current line
+        ghost_text = { enabled = true },
+      },
+    },
+    term = {
+      enabled = false,
+      keymap = { preset = "inherit" }, -- Inherits from top level `keymap` config when not set
+      sources = {},
+      completion = {
+        trigger = {
+          show_on_blocked_trigger_characters = {},
+          show_on_x_blocked_trigger_characters = nil, -- Inherits from top level `completion.trigger.show_on_blocked_trigger_characters` config when not set
+        },
+        -- Inherits from top level config options when not set
+        list = {
+          selection = {
+            -- When `true`, will automatically select the first item in the completion list
+            preselect = nil,
+            -- When `true`, inserts the completion item automatically when selecting it
+            auto_insert = nil,
+          },
+        },
+        -- Whether to automatically show the window when new completion items are available
+        menu = { auto_show = nil },
+        -- Displays a preview of the selected item on the current line
+        ghost_text = { enabled = nil },
       },
     },
     keymap = {
